45f567dd89f02912f91c3443192598d1cd7b6995
bplukasz <bp.lukasz@gmail.com>
2017-06-06 18:44:11 +0200

    Install libGDX

---
+++ desktop/src/space/invaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package space.invaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import space.invaders.SpaceInvadersGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceInvadersGame(), config);
+	}
+}
