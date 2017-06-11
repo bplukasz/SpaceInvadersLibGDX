20e77921ab45c26dad62e9a9a033315df8abaeda
bplukasz <bp.lukasz@gmail.com>
2017-06-11 06:13:34 +0200

    Odseparowanie logiki gry od widoku

--- core/src/space/invaders/SpaceInvadersGame.java
+++ core/src/space/invaders/SpaceInvadersGame.java
@@ -1,13 +1,23 @@
 package space.invaders;
 
+import java.util.LinkedList;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
+import space.invaders.entities.Alien;
+import space.invaders.entities.Bullet;
+import space.invaders.entities.Player;
+import space.invaders.infrastructure.Direction;
 import space.invaders.infrastructure.ObjectType;
+import space.invaders.screens.GameOverScreen;
 import space.invaders.screens.SplashScreen;
 
 public class SpaceInvadersGame extends Game {
@@ -22,70 +32,184 @@ public class SpaceInvadersGame extends Game {
 	private int playerBulletCounter;
 	private int alienBulletCounter;
 	private int level;
-	
-
-	public int getScore() {
-		return score;
-	}
+	private Stage stage;
+	private Player player;
+	private LinkedList<Alien> monsters;
+	private LinkedList<Bullet> bullets;
+	private Direction monstersDirection;
+	private Object monitor;
+	private float timeHelper;
 
 	@Override
 	public void create () {
+		this.setScreen(new SplashScreen(this));
+	}
+	
+	public void startGame(Stage stage) {
+		this.stage=stage;
+		initVariables();
+		initPlayer();
+		initMonsters();
+	}
+	
+	public void update() {
+		keyboardHandle();
+		moveAllMonsters();
+		checkAndHandleCollisions();	
+	}
+	
+	private void initVariables() {
+		monitor = new Object();
+		bullets = new LinkedList<Bullet>();
 		maxPlayerBulletsOnStage = 6;
 		maxAliensBulletsOnStage = 7;
 		playerBulletCounter=0;
 		alienBulletCounter=0;
 		level=1;
-		this.setScreen(new SplashScreen(this));
-	}
-	
-	@Override
-	public void dispose () {
-	}
-	
-	public boolean isPaused() {
-		return paused;
 	}
 
-	public void setPaused(boolean paused) {
-		this.paused = paused;
+	private void initPlayer() {
+		player = new Player(monitor);
+		stage.addActor(player);
 	}
 	
-	public void addScore() {
-		score+=1;
+	private void initMonsters() {
+		monsters = new LinkedList<Alien>();
+		for(int i=0; i<40; i++)
+		{
+			monsters.add(new Alien(63 + (i%10)*50, 600-(i/10)*50));
+		}
+		for(Alien monster: monsters){
+			stage.addActor(monster);
+		}
+		monstersDirection=Direction.Right;
 	}
 	
-	public void addScore(int scoreToAdd) {
-		score+=scoreToAdd;
+	private void moveAllMonsters() {
+		boolean widthBorderIsCrossedFlag = false;
+		boolean bottomBorderIsCrossedFlag = false;
+		for(Alien monster:monsters){
+			if(monster.widthBorderIsCrossed(this))
+			{
+				widthBorderIsCrossedFlag=true;
+				break;
+			}
+		}
+		
+		if(widthBorderIsCrossedFlag==true){
+			if(monstersDirection==Direction.Left)
+				monstersDirection=Direction.Right;
+			else if(monstersDirection==Direction.Right)
+				monstersDirection=Direction.Down;
+			else monstersDirection=Direction.Left;
+		}
+		
+		for(Alien monster:monsters){
+			if(monster.bottomBorderIsCrossed(this))
+			{
+				bottomBorderIsCrossedFlag=true;
+				break;
+			}
+		}
+		if(bottomBorderIsCrossedFlag){
+			gameOver();
+		}
+		
+		for(Alien monster:monsters){
+			monster.move(monstersDirection);
+		}
+		
+		if(monsters.isEmpty())nextLevel();
 	}
 	
-	public int getLevel() {
-		return level;
-	}
-
-	public void setLevel(int level) {
-		this.level = level;
+	private void checkAndHandleCollisions() {
+		LinkedList<Bullet> bulletsToRemove = new LinkedList<Bullet>();
+		LinkedList<Alien> monstersToRemove = new LinkedList<Alien>();
+		for(Bullet bullet: bullets)
+		{
+			for(Alien monster: monsters){
+				if(bullet.overlaps(monster))
+				{
+					bulletsToRemove.add(bullet);
+					monstersToRemove.add(monster);
+				}
+			}
+			if(bullet.gameBorderIsCrossed(this)){
+				bulletsToRemove.add(bullet);
+			}
+		}
+		for(Alien monster: monstersToRemove){
+			this.addScore();
+			monsters.remove(monster);
+			monster.remove();
+		}
+		for(Bullet bullet: bulletsToRemove){
+			bullets.remove(bullet);
+			bullet.remove();
+			this.decrementBulletCounter(ObjectType.PLAYER);
+		}
+		
+		synchronized(monitor){
+			monitor.notifyAll();
+		}
 	}
 	
-	public void setMaxPlayerBulletsOnStage(int maxPlayerBulletsOnStage) {
-		this.maxPlayerBulletsOnStage = maxPlayerBulletsOnStage;
+	private void nextLevel() {
+		initMonsters();
+		level++;
+		for(Alien monster: monsters){
+			monster.setAlienSpeed(level+2);
+		}
+		maxAliensBulletsOnStage=level+7;
+		maxPlayerBulletsOnStage=level+6;
 	}
-
-	public void setMaxAliensBulletsOnStage(int maxAliensBulletsOnStage) {
-		this.maxAliensBulletsOnStage = maxAliensBulletsOnStage;
-	}
-
 	
-	public void incrementBulletCounter(ObjectType whoCreateBullet){
+	private void gameOver() {
+		this.setScreen(new GameOverScreen(this));
+	}
+	
+	private void keyboardHandle(){
+		if(Gdx.input.isKeyPressed(Keys.A)){
+			if(!player.leftBorderIsCrossed(this))
+				player.move(Direction.Left);
+		}
+		if(Gdx.input.isKeyPressed(Keys.D)){
+			if(!player.rightBorderIsCrossed(this))
+				player.move(Direction.Right);			
+		}
+		if(Gdx.input.isKeyPressed(Keys.ESCAPE)){
+			Gdx.app.exit();
+		}
+		if(Gdx.input.isKeyPressed(Keys.SPACE)){
+			timeHelper += Gdx.graphics.getDeltaTime();
+			if(this.canICreateBullet(ObjectType.PLAYER))
+			{
+				if(timeHelper > 0.1)
+				{
+					Bullet bullet;
+					bullet = player.shot();
+					Thread thread = new Thread(bullet);
+					stage.addActor(bullet);
+					bullets.add(bullet);
+					thread.start();
+					timeHelper = 0;
+					this.incrementBulletCounter(ObjectType.PLAYER);
+				}
+			}
+		}
+	}
+	
+	private void incrementBulletCounter(ObjectType whoCreateBullet){
 		if(whoCreateBullet==ObjectType.ALIEN)alienBulletCounter++;
 		if(whoCreateBullet==ObjectType.PLAYER)playerBulletCounter++;
 	}
 	
-	public void decrementBulletCounter(ObjectType whoCreateBullet){
+	private void decrementBulletCounter(ObjectType whoCreateBullet){
 		if(whoCreateBullet==ObjectType.ALIEN)alienBulletCounter--;
 		if(whoCreateBullet==ObjectType.PLAYER)playerBulletCounter--;
 	}
 	
-	public boolean canICreateBullet(ObjectType who){
+	private boolean canICreateBullet(ObjectType who){
 		if(who==ObjectType.ALIEN){
 			if(alienBulletCounter<maxAliensBulletsOnStage)return true;
 		}
@@ -93,5 +217,38 @@ public class SpaceInvadersGame extends Game {
 			if(playerBulletCounter<maxPlayerBulletsOnStage)return true;
 		}
 		return false;
+	}
+
+	private void addScore() {
+		score+=1;
+	}
+	
+	private void addScore(int scoreToAdd) {
+		score+=scoreToAdd;
+	}
+	
+	//Getters and setters
+	public void setPaused(boolean paused) {
+		this.paused = paused;
+	}
+	
+	public int getLevel() {
+		return level;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	@Override
+	public void dispose () {
+	}
+	
+	public boolean isPaused() {
+		return paused;
 	}
 }
--- core/src/space/invaders/screens/GameplayScreen.java
+++ core/src/space/invaders/screens/GameplayScreen.java
@@ -1,41 +1,20 @@
 package space.invaders.screens;
 
-import java.util.LinkedList;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.List;
 
 import space.invaders.SpaceInvadersGame;
-import space.invaders.entities.Player;
-import space.invaders.infrastructure.Direction;
-import space.invaders.infrastructure.ObjectType;
-import space.invaders.entities.Alien;
-import space.invaders.entities.Bullet;
 
 public class GameplayScreen extends AbstractScreen {
-	
-	private Player player;
-	private LinkedList<Alien> monsters;
-	private LinkedList<Bullet> bullets;
-	private Direction monstersDirection;
 	private Label scoreLabel;
 	private Label levelLabel;
-	private Object monitor;
-	private float timeHelper;
-	
 	
 	public GameplayScreen(SpaceInvadersGame game) {
 		super(game);
-		monitor = new Object();
-		bullets = new LinkedList<Bullet>();
-		initPlayer();
-		initMonsters();
 		initScoreLabel();
 		initLevelLabel();
+		game.startGame(stage);
 	}
 	
 	private void initScoreLabel() {
@@ -56,152 +35,18 @@ public class GameplayScreen extends AbstractScreen {
 		stage.addActor(levelLabel);
 	}
 
-
+	private void updateLabels(){
+		scoreLabel.setText("Wynik: " + game.getScore());
+		levelLabel.setText("Level: " + game.getLevel());
+	}
+	
 	@Override
 	public void render(float delta){
 		super.render(delta);
-		update();
+		game.update();
+		updateLabels();
 		spriteBatch.begin();
 		stage.draw();
 		spriteBatch.end();
-	}
-	
-	private void update() {
-		keyboardHandle();
-		moveAllMonsters();
-		scoreLabel.setText("Wynik: " + game.getScore());
-		levelLabel.setText("Level: " + game.getLevel());
-		checkAndHandleCollisions();	
-	}
-
-	private void checkAndHandleCollisions() {
-		LinkedList<Bullet> bulletsToRemove = new LinkedList<Bullet>();
-		LinkedList<Alien> monstersToRemove = new LinkedList<Alien>();
-		for(Bullet bullet: bullets)
-		{
-			for(Alien monster: monsters){
-				if(bullet.overlaps(monster))
-				{
-					bulletsToRemove.add(bullet);
-					monstersToRemove.add(monster);
-				}
-			}
-			if(bullet.gameBorderIsCrossed(game)){
-				bulletsToRemove.add(bullet);
-			}
-		}
-		for(Alien monster: monstersToRemove){
-			game.addScore();
-			monsters.remove(monster);
-			monster.remove();
-		}
-		for(Bullet bullet: bulletsToRemove){
-			bullets.remove(bullet);
-			bullet.remove();
-			game.decrementBulletCounter(ObjectType.PLAYER);
-		}
-		
-		synchronized(monitor){
-			monitor.notifyAll();
-		}
-	}
-
-	private void moveAllMonsters() {
-		boolean widthBorderIsCrossedFlag = false;
-		boolean bottomBorderIsCrossedFlag = false;
-		for(Alien monster:monsters){
-			if(monster.widthBorderIsCrossed(game))
-			{
-				widthBorderIsCrossedFlag=true;
-				break;
-			}
-		}
-		
-		if(widthBorderIsCrossedFlag==true){
-			if(monstersDirection==Direction.Left)
-				monstersDirection=Direction.Right;
-			else if(monstersDirection==Direction.Right)
-				monstersDirection=Direction.Down;
-			else monstersDirection=Direction.Left;
-		}
-		
-		for(Alien monster:monsters){
-			if(monster.bottomBorderIsCrossed(game))
-			{
-				bottomBorderIsCrossedFlag=true;
-				break;
-			}
-		}
-		if(bottomBorderIsCrossedFlag){
-			gameOver();
-		}
-		
-		for(Alien monster:monsters){
-			monster.move(monstersDirection);
-		}
-		
-		if(monsters.isEmpty())nextLevel();
-	}
-
-	private void gameOver() {
-		game.setScreen(new GameOverScreen(game));
-	}
-
-	private void nextLevel() {
-		initMonsters();
-		game.setLevel(game.getLevel()+1);
-		for(Alien monster: monsters){
-			monster.setAlienSpeed(game.getLevel()+2);
-		}
-		game.setMaxAliensBulletsOnStage(game.getLevel()+7);
-		game.setMaxPlayerBulletsOnStage(game.getLevel()+6);
-	}
-
-	private void initPlayer() {
-		player = new Player(monitor);
-		stage.addActor(player);
-	}
-	
-	private void initMonsters() {
-		monsters = new LinkedList<Alien>();
-		for(int i=0; i<40; i++)
-		{
-			monsters.add(new Alien(63 + (i%10)*50, 600-(i/10)*50));
-		}
-		for(Alien monster: monsters){
-			stage.addActor(monster);
-		}
-		monstersDirection=Direction.Right;
-	}
-	
-	private void keyboardHandle(){
-		if(Gdx.input.isKeyPressed(Keys.A)){
-			if(!player.leftBorderIsCrossed(game))
-				player.move(Direction.Left);
-		}
-		if(Gdx.input.isKeyPressed(Keys.D)){
-			if(!player.rightBorderIsCrossed(game))
-				player.move(Direction.Right);			
-		}
-		if(Gdx.input.isKeyPressed(Keys.ESCAPE)){
-			Gdx.app.exit();
-		}
-		if(Gdx.input.isKeyPressed(Keys.SPACE)){
-			timeHelper += Gdx.graphics.getDeltaTime();
-			if(game.canICreateBullet(ObjectType.PLAYER))
-			{
-				if(timeHelper > 0.1)
-				{
-					Bullet bullet;
-					bullet = player.shot();
-					Thread thread = new Thread(bullet);
-					stage.addActor(bullet);
-					bullets.add(bullet);
-					thread.start();
-					timeHelper = 0;
-					game.incrementBulletCounter(ObjectType.PLAYER);
-				}
-			}
-		}
 	}
 }
